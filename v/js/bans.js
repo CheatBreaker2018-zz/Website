bbae27448497ace986ce8531ce199b445557a271
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-06-15 23:06:54 -0400

    Add files via upload

---
+++ v/js/bans.js
@@ -0,0 +1,35 @@
+var myList = [
+    {"Added": "","Username": "","Reason": "","Duration": ""}
+  ];
+  
+  function buildHtmlTable(selector) {
+    var columns = addAllColumnHeaders(myList, selector);
+  
+    for (var i = 0; i < myList.length; i++) {
+      var row$ = $('<tr/>');
+      for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+        var cellValue = myList[i][columns[colIndex]];
+        if (cellValue == null) cellValue = "";
+        row$.append($('<td/>').html(cellValue));
+      }
+      $(selector).append(row$);
+    }
+  }
+
+  function addAllColumnHeaders(myList, selector) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+  
+    for (var i = 0; i < myList.length; i++) {
+      var rowHash = myList[i];
+      for (var key in rowHash) {
+        if ($.inArray(key, columnSet) == -1) {
+          columnSet.push(key);
+          headerTr$.append($('<th/>').html(key));
+        }
+      }
+    }
+    $(selector).append(headerTr$);
+  
+    return columnSet;
+  }
