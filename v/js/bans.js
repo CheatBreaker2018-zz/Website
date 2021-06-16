3e930997b92583e0827ad650bc7809e9920707d9
ArcaneCiCi <59527329+ArcaneCiCi@users.noreply.github.com>
2021-06-16 12:08:21 -0400

    Add files via upload

--- v/js/bans.js
+++ v/js/bans.js
@@ -1,35 +1,140 @@
 var myList = [
-    {"Added": "6-16-2021","Username": "Gollem","Reason": "Requested","Duration": "Forever"}
-  ];
-  
-  function buildHtmlTable(selector) {
-    var columns = addAllColumnHeaders(myList, selector);
-  
-    for (var i = 0; i < myList.length; i++) {
-      var row$ = $('<tr/>');
-      for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-        var cellValue = myList[i][columns[colIndex]];
-        if (cellValue == null) cellValue = "";
-        row$.append($('<td/>').html(cellValue));
-      }
-      $(selector).append(row$);
-    }
-  }
+  {"Added": "6-16-2021","Username": "HeySchat","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "pfscheck","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Ivarklugt","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "ItsVops","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "STOCKED ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "sownah ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "sownah777 ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "LosingYou ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Derized ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Sxizure ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "RingPvP ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "casino300 ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Anabubbles ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "wasted999 ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Saranicole99 ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Kilkor ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "PitereKTM ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "FaNyNeLox ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Sownaha ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "dragonflitter ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "kocvr ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "I1I1I1I1I1I ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "POZDRO ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "zmuwmodly ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "1283819ukds ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "666szatan ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "FrameTiger ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "CommanderRod ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "_HyperTR ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "_432 ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "deuu ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "SadMercy ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "TNTapple1234 ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "madkeve ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Imabrit ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "otherwhile ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Nodebuff_Nub ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "bing__lin_666 ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "TimberDND ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "LucidThinking ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Elations ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Repossessions ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Imperatively ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "JustAMonster ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "Erasur  ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "ItsJustDamian  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Raeny  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Vonripper41  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "aDerpyNiqqa  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "typhoonk99  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "xKhep  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "StormG0D  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "whosealtisthis  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Flemos  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "BackInTheOldDay  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "GANGMEMBERPICKLE  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "ImRice_  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Seinpai  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "minevern123  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Arima_a  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "unicornLover429  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "SportCenter  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Trinox_Pvp  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Sobcakes ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Jobun  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "mariwana  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Bianca  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "ScoobyPVP1  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "SOBEYS  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "YouFuckuh  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "sensibel  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Oculism_  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "ImNeyoBack  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "NextTimePanda  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "ySlim  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "itsPqvlos  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "QuotenSchwede  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "nubway  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Xillions  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "AndrewMorgan  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Fenghuang  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "niasyn  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Sikoreq  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "DeasterPvP  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "11cpsL0L  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Knilezz  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "DaddyNubway  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Kayzuke  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "STREETEX  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "KimbleKeys  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "imgarrex  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "FNSG0D  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "SADMEMBERGAMEHD  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "HellRed  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Delfboy  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "flecher9753  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "RainbewDood  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "HM0  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "GayDrag0n  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "OQ7  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "IchBinShiny  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "6642  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "lord_black05  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "franco060104  ","Reason": "Cheating","Duration": "Forever"},    
+  {"Added": "6-16-2021","Username": "Outsider21  ","Reason": "Cheating","Duration": "Forever"},
+  {"Added": "6-16-2021","Username": "nxez  ","Reason": "Cheating","Duration": "Forever"}
+];
 
-  function addAllColumnHeaders(myList, selector) {
-    var columnSet = [];
-    var headerTr$ = $('<tr/>');
-  
-    for (var i = 0; i < myList.length; i++) {
-      var rowHash = myList[i];
-      for (var key in rowHash) {
-        if ($.inArray(key, columnSet) == -1) {
-          columnSet.push(key);
-          headerTr$.append($('<th/>').html(key));
-        }
+function buildHtmlTable(selector) {
+  var columns = addAllColumnHeaders(myList, selector);
+
+  for (var i = 0; i < myList.length; i++) {
+    var row$ = $('<tr/>');
+    for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+      var cellValue = myList[i][columns[colIndex]];
+      if (cellValue == null) cellValue = "";
+      row$.append($('<td/>').html(cellValue));
+    }
+    $(selector).append(row$);
+  }
+}
+
+function addAllColumnHeaders(myList, selector) {
+  var columnSet = [];
+  var headerTr$ = $('<tr/>');
+
+  for (var i = 0; i < myList.length; i++) {
+    var rowHash = myList[i];
+    for (var key in rowHash) {
+      if ($.inArray(key, columnSet) == -1) {
+        columnSet.push(key);
+        headerTr$.append($('<th/>').html(key));
       }
     }
-    $(selector).append(headerTr$);
-  
-    return columnSet;
   }
+  $(selector).append(headerTr$);
+
+  return columnSet;
+}
